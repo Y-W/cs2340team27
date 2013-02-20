212ee8c60d9c7d38a1f68d87ce18bed2240360a9
wyijie93@gmail.com <wyijie93@gmail.com@5347a7c5-ffa8-7b6e-7fcd-912530afc9ca>
2013-02-20 02:20:52 +0000

    Add Communication Class. YW

---
+++ src/edu/gatech/cs2340/team27/lostandfound/model/Communication.java
@@ -0,0 +1,23 @@
+package edu.gatech.cs2340.team27.lostandfound.model;
+
+/*
+ * Handles communication
+ * 
+ * @author Yijie Wang
+ * @version 0.1
+ */
+public class Communication {
+	private static Communication onlyInstance;
+
+	/*
+	 * returns the only instance of Communication. If there's no instance ever created, then create one.
+	 * 
+	 * @return the only instance of Communication
+	 */
+	public static Communication getInstance(){
+		if(onlyInstance==null){
+			onlyInstance=new Communication();
+		}
+		return onlyInstance;
+	}
+}
