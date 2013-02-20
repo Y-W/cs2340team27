5ffc5b9c1248cff1d1739729dd14a1908c1873f7
yiqic1993@gmail.com <yiqic1993@gmail.com@5347a7c5-ffa8-7b6e-7fcd-912530afc9ca>
2013-02-20 03:02:59 +0000

    Create front end login attempt.

--- src/edu/gatech/cs2340/team27/lostandfound/Login.java
+++ src/edu/gatech/cs2340/team27/lostandfound/Login.java
@@ -1,8 +1,12 @@
 package edu.gatech.cs2340.team27.lostandfound;
 
-import android.os.Bundle;
+import edu.gatech.cs2340.team27.lostandfound.model.Communication.LoginStatus;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
 
 public class Login extends Activity {
 
@@ -17,6 +21,14 @@ public class Login extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.login, menu);
 		return true;
+	}
+	
+	public void loginAttempt(View view) {
+		EditText username = (EditText) findViewById(R.id.editText1);
+		EditText password = (EditText) findViewById(R.id.editText2);
+		String userText = username.getText().toString();
+		String psText = password.getText().toString();
+		LoginStatus status = edu.gatech.cs2340.team27.lostandfound.model.Communication.loginAttempt(userText, psText);
 	}
 
 }
