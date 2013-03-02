e6be065b758c6550e8ff6ea8adb22ebb49ea626a
djyhu1226@gmail.com <djyhu1226@gmail.com@5347a7c5-ffa8-7b6e-7fcd-912530afc9ca>
2013-03-02 02:05:17 +0000

    add javadoc

--- src/edu/gatech/cs2340/team27/lostandfound/HomePage.java
+++ src/edu/gatech/cs2340/team27/lostandfound/HomePage.java
@@ -9,33 +9,55 @@ import android.view.View;
 public class HomePage extends Activity {
 
 	@Override
+	/**
+	 * build in method
+	 */
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_home_page);
 	}
 
 	@Override
+	/**
+	 * build in method
+	 */
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.home_page, menu);
 		return true;
 	}
-	
+	/**
+	 * loserAttempt
+	 * directs to lostItemInput page
+	 * @param view
+	 */
 	public void loserAttempt(View view){
 		Intent intent = new Intent(this,LostItemInput.class);
 		startActivity(intent);
 	}
-	
+	/**
+	 * founderAttempt
+	 * directs to FoundItemInput page
+	 * @param view
+	 */
 	public void founderAttempt(View view){
 	//	Intent intent = new Intent(this,.class);
 	//	startActivity(intent);
 	}
-	
+	/**
+	 * lostinfoAttempt
+	 * directs to lostItem page
+	 * @param view
+	 */
 	public void lostinfoAttempt(View view){
 		Intent intent = new Intent(this,LostItem.class);
 		startActivity(intent);
 	}
-	
+	/**
+	 * foundinfoAttempt
+	 * directs to FoundItem page
+	 * @param view
+	 */
 	public void foundinfoAttempt(View view){
 	//	Intent intent = new Intent(this,.class);
 	//	startActivity(intent);
