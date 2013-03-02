59e6bcffbcb8b2a487958467a6847aa9432058ae
yiqic1993@gmail.com <yiqic1993@gmail.com@5347a7c5-ffa8-7b6e-7fcd-912530afc9ca>
2013-03-02 02:42:42 +0000

    Initialize item

--- src/edu/gatech/cs2340/team27/lostandfound/data/Item.java
+++ src/edu/gatech/cs2340/team27/lostandfound/data/Item.java
@@ -7,11 +7,26 @@ public class Item {
 	private ItemStatus status;
 	private String name;
 	private String location;
+	private String description;
 	private Date lostDate;
 	private Date foundDate;
 	private Date resolvedDate;
 	private User loser;
 	private User founder;
+
+	public Item(ItemStatus status, String name, String location, String description, Date date, User user) {
+		this.status = status;
+		this.location = location;
+		this.description = description;
+		if(status == ItemStatus.LOST) {
+			lostDate = date;
+			loser = user;
+		}
+		else {
+			foundDate = date;
+			founder = user;
+		}
+	}
 	
 	public enum ItemStatus{
 		LOST,
