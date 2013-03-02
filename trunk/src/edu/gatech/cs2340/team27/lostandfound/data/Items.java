ecbc091f79ee32971f435aa6f3f0ff4967687be2
yiqic1993@gmail.com <yiqic1993@gmail.com@5347a7c5-ffa8-7b6e-7fcd-912530afc9ca>
2013-03-02 02:42:42 +0000

    Initialize item

--- trunk/src/edu/gatech/cs2340/team27/lostandfound/data/Items.java
+++ trunk/src/edu/gatech/cs2340/team27/lostandfound/data/Items.java
@@ -1,6 +1,9 @@
 package edu.gatech.cs2340.team27.lostandfound.data;
 
 import java.util.ArrayList;
+import java.util.Date;
+
+import edu.gatech.cs2340.team27.lostandfound.data.Item.ItemStatus;
 
 public class Items {
 	
@@ -16,5 +19,9 @@ public class Items {
 	
 	public void initialize(ArrayList<Item> items) {
 		list = items;
+	}
+	
+	public void addItem(ItemStatus status, String name, String location, String description, Date date, User user) {
+		list.add(new Item(status, name, location, description, date, user));
 	}
 }
