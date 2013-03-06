8245db026e0fd643064ed523cd4444399780a8cd
yiqic1993@gmail.com <yiqic1993@gmail.com@5347a7c5-ffa8-7b6e-7fcd-912530afc9ca>
2013-03-06 03:25:08 +0000

    javadoc

--- trunk/src/edu/gatech/cs2340/team27/lostandfound/data/User.java
+++ trunk/src/edu/gatech/cs2340/team27/lostandfound/data/User.java
@@ -49,15 +49,11 @@ public class User {
 		return email;
 	}
 	
+	/**
+	 * Getter
+	 * @return If it is locked
+	 */
 	public boolean isLocked(){
 		return isLocked;
-	}
-	
-	public void lock() {
-		isLocked = true;
-	}
-	
-	public void unlock() {
-		isLocked = false;
 	}
 }
