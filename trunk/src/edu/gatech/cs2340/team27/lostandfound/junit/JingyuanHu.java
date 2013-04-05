c6fc0916a3f245fd96710005c550a0921e3edf2b
djyhu1226@gmail.com <djyhu1226@gmail.com@5347a7c5-ffa8-7b6e-7fcd-912530afc9ca>
2013-04-05 21:02:55 +0000

    junit

---
+++ trunk/src/edu/gatech/cs2340/team27/lostandfound/junit/JingyuanHu.java
@@ -0,0 +1,37 @@
+package edu.gatech.cs2340.team27.lostandfound.junit;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.Date;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import edu.gatech.cs2340.team27.lostandfound.data.Item;
+import edu.gatech.cs2340.team27.lostandfound.data.Items;
+import edu.gatech.cs2340.team27.lostandfound.data.Item.ItemStatus;
+import edu.gatech.cs2340.team27.lostandfound.data.User;
+import edu.gatech.cs2340.team27.lostandfound.model.Communication;
+
+
+public class JingyuanHu {
+
+	@Before
+	public void setUp() throws Exception {
+		Communication.debug = true;
+	}
+
+	@Test
+	public void test() throws IOException, ClassNotFoundException, ParseException {
+		Item a = new Item(ItemStatus.FOUND,"APPLE","COC","An Apple",new Date(),new User("name", "123", "emaila", false),Item.Category.OTHER);
+		Item b = new Item(ItemStatus.LOST,"apple","COC","apple",new Date(),new User("nameb", "1111", "emailb", false),Item.Category.OTHER);
+		Item c = new Item(ItemStatus.LOST,"Banana","GT","A banana",new Date(),new User("namec", "1234", "emailc", false),Item.Category.OTHER);
+		assertTrue(Items.getInstance().match(a, b));
+		assertFalse(Items.getInstance().match(a, c));
+		
+		
+	}
+
+}
